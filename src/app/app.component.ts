e590a326fa11b76ac36ca800e3c5d234f765d3ab
22ARW XPX <22arwxpx@22ARWs-MacBook-Pro.local>
2018-12-16 12:54:02 -0600

    SerialPort is working now working on list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { } from 'electron';
 import * as Serialport from 'serialport';
+import { SerialService } from './serial.service';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +11,32 @@ import * as Serialport from 'serialport';
 export class AppComponent implements OnInit {
 
   title = 'electron-angular-serialport';
+  comPorts = [];
 
-  constructor() {
+  constructor(private serial: SerialService) {
     let isElectron: boolean = window && window['process'] && window['process'].type;
 
     if (isElectron) {
       let serialport: typeof Serialport = window['require']('serialport');
       let app: Electron.App = window['require']('electron').remote;
       console.log(serialport, app, window['process']);
-   }
+    }
   }
 
   ngOnInit() {
-    console.log(Serialport.list());
+    this.getPorts();
+    console.log(this.comPorts);
+  }
+
+  // getPorts() {
+  //   this.serial.serialPort.list(function (err, ports) {
+  //     ports.forEach(function(port) {
+  //       this.comPorts.push(port);
+  //     });
+  //   });
+  // }
+
+  getPorts() {
+    this.comPorts = this.serial.serialPort.list();
   }
 }
--- src/app/serial.service.ts
+++ src/app/serial.service.ts
@@ -6,11 +6,8 @@ import * as SerialPort from 'serialport';
 })
 export class SerialService {
 
-  serialPort: typeof SerialPort;
+  serialPort: typeof SerialPort = window['require']('serialport');
 
-  constructor() {
-    if (this.serialPort()) {
-      // this.serialPort = window.require('serialport');
-    }
-  }
+  constructor() { }
+
 }
